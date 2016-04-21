d5edf3b22b6b265a5e14cae9d394e7dc13759390
Eric P. Katz <ericpkatz@gmail.com>
2016-04-21 17:12:50 -0400

    see if you can get inject to work on ci

--- tests/browser/foo.test.js
+++ tests/browser/foo.test.js
@@ -1,6 +1,13 @@
 describe('foo', function(){
   beforeEach(module('FullstackGeneratedApp'));
 
+  var $httpBackend;
+  var $rootScope;
+  beforeEach('Get tools', inject(function (_$httpBackend_, _$rootScope_) {
+      $httpBackend = _$httpBackend_;
+      $rootScope = _$rootScope_;
+  }));
+
   it('works', function(){
     expect(1 + 1).to.equal(2);
   

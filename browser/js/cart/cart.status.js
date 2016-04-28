6906957fa065e43eccd37292076b6891040316f7
Eric P. Katz <ericpkatz@gmail.com>
2016-04-28 09:15:29 -0400

    move some stuff to templates

--- browser/js/cart/cart.status.js
+++ browser/js/cart/cart.status.js
@@ -1,6 +1,6 @@
 app.directive('cartStatus', function(CartService, $state){
   return {
-    template: '<li ng-if="hasCart()"><a ui-sref="cart" >Cart ({{ itemCount() }} items)</a></li>',
+    templateUrl: 'js/cart/cart.status.html',
     replace: true,
     link: function(scope){
       scope.itemCount = function(){

f092f86d774820e5474a8a140761837599b4dabf
Eric P. Katz <ericpkatz@gmail.com>
2016-04-29 15:21:55 -0400

    refactor code

--- server/app/routes/products/index.js
+++ server/app/routes/products/index.js
@@ -29,42 +29,26 @@ router.post('/', ensureAuthenticated, function (req, res, next) {
 });
 
 router.post('/:id/reviews/', ensureAuthenticated, function (req, res, next) {
-  Product.findById(req.params.id)
-    .then(function(product){
-      var existing = product.reviews.filter(function(review){
-        return review.user.toString() === req.user.id;
-      });
-      if(existing.length > 0){
-        return res.sendStatus(404);
-      }
-      product.reviews.push({user: req.user._id, rating: req.body.rating});
-      return product.save();
-    })
-    .then(function(product){
-      var existing = product.reviews.filter(function(review){
-        return review.user.toString() === req.user.id;
-      });
-      res.send(existing[0]);
-    });
+  var review = {
+    user: req.user,
+    rating: req.body.rating,
+    _id: req.params.reviewId
+  };
+  Product.addOrUpdateReview(req.params.id, review)
+    .then(function(){
+      res.send(review);
+    }, next);
 });
 
 router.put('/:id/reviews/:reviewId', ensureAuthenticated, function (req, res, next) {
-  Product.findById(req.params.id)
-    .then(function(product){
-      var existing = product.reviews.filter(function(review){
-        return review.user.toString() === req.user.id && review._id.toString() === req.params.reviewId;
-      });
-      if(existing.length === 0){
-        return res.sendStatus(404);
-      }
-      existing[0].rating = req.body.rating;
-      return product.save();
-    })
-    .then(function(product){
-      var existing = product.reviews.filter(function(review){
-        return review.user.toString() === req.user.id;
-      });
-      res.send(existing[0]);
-    });
+  var review = {
+    user: req.user,
+    rating: req.body.rating,
+    _id: req.params.reviewId
+  };
+  Product.addOrUpdateReview(req.params.id, review)
+    .then(function(){
+      res.send(review);
+    }, next);
 });
 
--- server/db/models/product.js
+++ server/db/models/product.js
@@ -15,9 +15,34 @@ var schema = new mongoose.Schema({
   reviews: [ reviewSchema ]
 });
 
+schema.statics.addOrUpdateReview = function(productId, review){
+  return this.findById(productId)
+    .then(function(product){
+      return product.addOrUpdateReview(review);
+    });
+};
+
+
+schema.methods.addOrUpdateReview = function(review){
+  if(review._id)
+    return this.updateReview(review);
+  else
+    return this.addReview(review);
+};
+
+schema.methods.updateReview = function(review){
+  var _review = this.reviews.filter(function(r){
+    return review._id == r._id && r.user == review.user.id;
+  })[0];
+  if(!_review)
+    throw 'review not found for user and id';
+  _review.rating = review.rating;
+  return this.save();
+};
+
 schema.methods.addReview = function(review){
-  //TO DO validate that user can make a review!
   this.reviews.push(review);
+  return this.save();
 };
 
 
